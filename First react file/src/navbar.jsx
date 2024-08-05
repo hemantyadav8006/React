c4417cc7cd864f6925f336bf5034b8942a241d64
Hemant Kumar Yadav <55074926+hemantyadav8006@users.noreply.github.com>
2024-08-05 22:16:12 +0530

    updated

---
+++ First react file/src/navbar.jsx
@@ -0,0 +1,21 @@
+
+
+function Navbar(){
+    return (
+        <header>
+            <nav>
+                <div class="logo">
+                    <i class="fa-solid fa-code"></i>
+                    
+                </div>
+                <div class="links">
+                    <a href="#" class="active">Home</a>
+                    <a href="#abor">About</a>
+                    <a href="#">Contact Me</a>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar;
