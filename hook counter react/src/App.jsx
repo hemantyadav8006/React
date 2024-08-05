68dedf81faac705211e751961f95c1b61fb77ef1
Hemant Kumar Yadav <55074926+hemantyadav8006@users.noreply.github.com>
2024-08-06 02:26:08 +0530

    updated app.jsx

--- hook counter react/src/App.jsx
+++ hook counter react/src/App.jsx
@@ -12,6 +12,23 @@ function App() {
     if (counter < 20) {
       counter = counter + 1;
       setCounter(counter)
+      setCounter(counter)
+      setCounter(counter)
+      setCounter(counter)
+      /*
+      Interview question: when we call multiple setCounter's 
+
+      Detailed explaination of why does first syntax only updates the count once:
+      Initial State: Assume count is initially 69.
+      First Call: setCount(count + 1) schedules a state update to set count to 70 (69 + 1).
+      Second Call: setCount(count + 1) schedules another state update to set count to 70 (69 + 1), because count is still 69 in this scope.
+      Third Call: setCount(count + 1) schedules yet another state update to set count to 70 (69 + 1), again because count is still 69 in this scope.
+
+      In case of functional updater syntax React ensures that changes are made to the latest state of the count hence each function gets access to the latest state of the count variable:
+      First Call: setCount(count =>count+1) schedules a state update to set count to 70
+      Second Call : schedules a state update to set count to (70+1) because count is now 70 in this scope and so on...
+      */
+     
       console.log(`Add value`, counter); 
     } else {
       alert('You cannot add more than 20 !');
