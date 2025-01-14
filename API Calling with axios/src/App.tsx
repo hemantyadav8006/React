5de5c88199fa4e0aa4650ffe41c0331e125d8254
Hemant Kumar Yadav <55074926+hemantyadav8006@users.noreply.github.com>
2025-01-14 10:35:54 +0530

    add API calling with axios project with React, TypeScript

---
+++ API Calling with axios/src/App.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+interface TodoItems {
+  body: string;
+  id: number;
+  title: string;
+  userId: number;
+}
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<TodoItems[]>([]);
+  useEffect(() => {
+    axios
+      .get<TodoItems[]>("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        setTodos(response.data);
+        console.log(response.data);
+      });
+  }, []);
+  return (
+    <>
+      <div>
+        <h1>
+          Request Fetched from
+          <code>"https://jsonplaceholder.typicode.com/posts"</code>
+        </h1>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              id: {todo.id}, body: {todo.body}, title: {todo.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default App;
