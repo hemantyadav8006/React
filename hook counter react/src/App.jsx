0e0714da3aaf989e12d49b49d54151e07f805074
hemantyadav8006 <hemant.kumar@kollegeapply.com>
2025-04-16 17:53:37 +0530

    syntax change

--- hook counter react/src/App.jsx
+++ hook counter react/src/App.jsx
@@ -1,20 +1,17 @@
 import { useState } from "react";
 
 function App() {
-
-  let [counter, setCounter] = useState(15);
-
+  let [counter, setCounter] = useState(10);
 
   // let counter = 15;
-
 
   const addValue = () => {
     if (counter < 20) {
       counter = counter + 1;
-      setCounter(counter)
-      setCounter(counter)
-      setCounter(counter)
-      setCounter(counter)
+      setCounter(counter);
+      setCounter(counter);
+      setCounter(counter);
+      setCounter(counter);
       /*
       Interview question: when we call multiple setCounter's 
 
@@ -28,42 +25,37 @@ function App() {
       First Call: setCount(count =>count+1) schedules a state update to set count to 70
       Second Call : schedules a state update to set count to (70+1) because count is now 70 in this scope and so on...
       */
-     
-      console.log(`Add value`, counter); 
+
+      console.log(`Add value`, counter);
     } else {
-      alert('You cannot add more than 20 !');
+      alert("You cannot add more than 20 !");
     }
-  }
+  };
 
   const removeValue = () => {
     if (counter > 0) {
       counter = counter - 1;
-      setCounter(counter)
+      setCounter(counter);
       console.log(`Remove value`, counter);
     } else {
-      alert('You cannot remove less than 0 !')
+      alert("You cannot remove less than 0 !");
     }
-  }
-
+  };
 
   return (
     <>
-    <p>UI updation ko react control krta h by the help of hooks! {counter}</p>
+      <p>UI updation ko react control krta h by the help of hooks! {counter}</p>
       <h1>Starting hooks</h1>
       <h2>Counter value: {counter}</h2>
 
-      <button
-      onClick={addValue}
-      >Add value by 1 : {counter}</button>
+      <button onClick={addValue}>Add value by 1 : {counter}</button>
       <br />
       <br />
-      <button
-      onClick={removeValue}
-      >Remove value by 1: {counter}</button>
+      <button onClick={removeValue}>Remove value by 1: {counter}</button>
 
       <p>Footer: {counter}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
